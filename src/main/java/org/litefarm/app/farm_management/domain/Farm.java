1dd9979fcbf3ae40eab9fdc604e840c222ed8257
atrevino89 <thxv3n0lvl@pm.me>
2025-06-24 08:00:08 +0200

    feat: add Farm aggregate root and tests

---
+++ src/main/java/org/litefarm/app/farm_management/domain/Farm.java
@@ -0,0 +1,99 @@
+package org.litefarm.app.farm_management.domain;
+
+import java.util.UUID;
+
+// Aggregate root
+public class Farm {
+    private final UUID uuid;
+    private final String name;
+    private final String phoneNumber;
+    private final String location;
+    private final String address;
+    private final String units;
+    private final String currency;
+    private final String image;
+
+    private Farm(Builder builder) {
+        String exceptionMessage = "Farm %s is required";
+        if (builder.uuid == null) throw new IllegalArgumentException(String.format(exceptionMessage, "uuid"));
+        if (builder.name == null || builder.name.isBlank())
+            throw new IllegalArgumentException(String.format(exceptionMessage, "name"));
+        if (builder.phoneNumber == null)
+            throw new IllegalArgumentException(String.format(exceptionMessage, "phone number"));
+        if (builder.location == null) throw new IllegalArgumentException(String.format(exceptionMessage, "location"));
+        if (builder.address == null) throw new IllegalArgumentException(String.format(exceptionMessage, "address"));
+        if (builder.units == null) throw new IllegalArgumentException(String.format(exceptionMessage, "units"));
+        if (builder.currency == null) throw new IllegalArgumentException(String.format(exceptionMessage, "currency"));
+        if (builder.image == null) throw new IllegalArgumentException(String.format(exceptionMessage, "image"));
+
+        this.uuid = builder.uuid;
+        this.name = builder.name;
+        this.phoneNumber = builder.phoneNumber;
+        this.location = builder.location;
+        this.address = builder.address;
+        this.units = builder.units;
+        this.currency = builder.currency;
+        this.image = builder.image;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public static class Builder {
+        private UUID uuid;
+        private String name;
+        private String phoneNumber;
+        private String location;
+        private String address;
+        private String units;
+        private String currency;
+        private String image;
+
+        public Builder uuid(UUID uuid) {
+            this.uuid = uuid;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+
+        }
+
+        public Builder location(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder units(String units) {
+            this.units = units;
+            return this;
+        }
+
+        public Builder currency(String currency) {
+            this.currency = currency;
+            return this;
+        }
+
+        public Builder image(String image) {
+            this.image = image;
+            return this;
+        }
+
+        public Farm build() {
+            return new Farm(this);
+        }
+
+    }
+}
---
+++ src/test/java/org/litefarm/app/farm_management/domain/FarmTest.java
@@ -0,0 +1,161 @@
+package org.litefarm.app.farm_management.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FarmTest {
+    private UUID uuid;
+    private String name;
+    private String phoneNumber;
+    private String location;
+    private String address;
+    private String units;
+    private String currency;
+    private String image;
+
+
+    @BeforeEach
+    public void init() {
+        this.uuid = UUID.randomUUID();
+        this.name = "San Miguel Farm";
+        this.phoneNumber = "+598 12345678";
+        this.location = "barn area";
+        this.address = "Montemorelos, Pilares, 65515 Los Pilares, N.L., Mexico";
+        this.units = "meters";
+        this.currency = "USD";
+        this.image = "http://s3.com/123j12";
+
+    }
+
+    @Test
+    void givenFarmWithValidProps_whenAttemptingToCreateFarm_thenFarmIsCreated() {
+        Farm farm = new Farm.Builder()
+                .uuid(this.uuid)
+                .name(this.name)
+                .phoneNumber(this.phoneNumber)
+                .location(this.location)
+                .address(this.address)
+                .units(this.units)
+                .currency(this.currency)
+                .image(this.image)
+                .build();
+
+        assertNotNull(farm);
+    }
+
+    @Test
+    void givenFarmWithNoName_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    //.name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    .address(this.address)
+                    .units(this.units)
+                    .currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoPhoneNumber_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    //.phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    .address(this.address)
+                    .units(this.units)
+                    .currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoLocation_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    //.location(this.location)
+                    .address(this.address)
+                    .units(this.units)
+                    .currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoAddress_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    //.address(this.address)
+                    .units(this.units)
+                    .currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoUnits_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    .address(this.address)
+                    //.units(this.units)
+                    .currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoCurrency_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    .address(this.address)
+                    .units(this.units)
+                    //.currency(this.currency)
+                    .image(this.image)
+                    .build();
+        });
+    }
+
+    @Test
+    void givenFarmWithNoImage_whenAttemptingToCreateFarm_thenIllegalArgumentExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Farm.Builder()
+                    .uuid(this.uuid)
+                    .name(this.name)
+                    .phoneNumber(this.phoneNumber)
+                    .location(this.location)
+                    .address(this.address)
+                    .units(this.units)
+                    .currency(this.currency)
+                    //.image(this.image)
+                    .build();
+        });
+    }
+}
